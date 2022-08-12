54937116f8af9a0321f74eb3026186da103ddadc
sathish1980 <kumar.sathish189@gmail.com>
2022-08-12 06:41:58 +0530

    scroll webtable and checkbox

---
+++ SeleniumBasics/CheckboxandRadiobutton.py
@@ -0,0 +1,39 @@
+import time
+
+import pyautogui
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common import by
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from webdriver_manager.chrome import ChromeDriverManager
+class checboxandradiobutton():
+
+    global driver
+
+    def Launch_Browser(self, web=None):
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=web)
+        self.driver.get("https://leafground.com/checkbox.xhtml")
+        # self.driver.get("https://www.ebay.com/")
+        # self.driver.get("https://leafground.com/drag.xhtml")
+        self.driver.maximize_window()
+    def checkboxvalidation(self,actualcoursname):
+
+        self.driver.find_element(By.XPATH,value="//*[@id='j_idt87:j_idt89']//div[2]").click()
+        self.driver.find_element(By.XPATH, value="//*[@id='j_idt87:j_idt91']//div[2]").click()
+        totalvalues=self.driver.find_elements(By.XPATH,value="//*[@id='j_idt87:basic']//tbody//tr//td")
+        size=len(totalvalues)
+        for eachvalue in range(1,size+1):
+            coursename=self.driver.find_element(By.XPATH, value="//*[@id='j_idt87:basic']//tbody//tr//td["+str(eachvalue)+"]//label").text
+            if coursename in actualcoursname:
+                self.driver.find_element(By.XPATH, value="//*[@id='j_idt87:basic']//tbody//tr//td[" + str(
+                    eachvalue) + "]//div[2]").click()
+
+        print(self.driver.find_element(By.XPATH, value="//*[@id='j_idt87:j_idt102']//div[2]").is_enabled())
+        #self.driver.find_element(By.XPATH, value="//*[@id='j_idt87:j_idt102']//div[2]").click()
+
+
+obj=checboxandradiobutton()
+obj.Launch_Browser()
+obj.checkboxvalidation(["Python","C-Sharp","Java","Javascript"])
--- SeleniumBasics/WebTable.py
+++ SeleniumBasics/WebTable.py
@@ -20,29 +20,31 @@ class webtable():
         self.driver.maximize_window()
 
     def webtablevalidation(self,SearchCountry,QualifiedStatus):
+        xpathvariable="//div[@class='ui-datatable-scrollable-body']//table//tbody//tr"
         Pagenumber=self.driver.find_elements(by=By.XPATH,value="//*[@class='ui-paginator-pages']//a")
         TotalPage=len(Pagenumber)
         for eachvalue1 in range(1, TotalPage + 1):
             if eachvalue1>1:
                 self.driver.find_element(by=By.XPATH, value="//*[@class='ui-paginator-pages']//a["+str(eachvalue1)+"]").click()
                 time.sleep(1)
-            EachRow=self.driver.find_elements(by=By.XPATH,value="//div[@class='ui-datatable-scrollable-body']//table//tbody//tr")
+            #EachRow=self.driver.find_elements(by=By.XPATH,value="//div[@class='ui-datatable-scrollable-body']//table//tbody//tr")
+            EachRow=self.driver.find_elements(by=By.XPATH,value=xpathvariable)
             TablerowSize=len(EachRow)
-            print(TablerowSize)
+
             for eachvalue in range(1,TablerowSize+1):
                 #time.sleep(2)
                 CountryName=self.driver.find_element(by=By.XPATH,
-                                          value="//div[@class='ui-datatable-scrollable-body']//table//tbody//tr["+ str(eachvalue) +"]//td[2]//span[3]").text
+                                          value=xpathvariable+"["+ str(eachvalue) +"]//td[2]//span[3]").text
                 if CountryName == SearchCountry:
                    #time.sleep(2)
                    Status= self.driver.find_element(by=By.XPATH,
-                                             value="//div[@class='ui-datatable-scrollable-body']//table//tbody//tr[" + str(
+                                             value=xpathvariable+"[" + str(
                                                  eachvalue) + "]//td[5]//span[2]").text
                    if Status==QualifiedStatus:
                        print(QualifiedStatus)
                        #time.sleep(2)
                        Name = self.driver.find_element(by=By.XPATH,
-                                                        value="//div[@class='ui-datatable-scrollable-body']//table//tbody//tr[" + str(
+                                                        value=xpathvariable+"[" + str(
                                                             eachvalue) + "]//td[1]").text
                        Representative = self.driver.find_element(by=By.XPATH,
                                                        value="//div[@class='ui-datatable-scrollable-body']//table//tbody//tr[" + str(
@@ -55,5 +57,5 @@ class webtable():
 
 obj= webtable()
 obj.Launch_Browser()
-obj.webtablevalidation("India","UNQUALIFIED")
+obj.webtablevalidation("Germany","RENEWAL")
 
